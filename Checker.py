71deb412b02bb79980db26df526be1bf0777f803
lizhihao <3085292151@qq.com>
2015-04-03 09:40:25 +0800

    init

---
+++ Checker.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+
+"""
+Author:lizhihao
+"""
+import sys, threading, xlrd, shutil, os, time
+from PyQt4 import QtGui
+from PyQt4.QtGui import QDialog
+from PyQt4.QtCore import pyqtSignature, pyqtSignal, QObject
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+from PIL import Image
+
+from Ui_Checker import Ui_Dialog
+
+
+class CheckTool(QDialog, Ui_Dialog, QObject):
+
+    printSignal = pyqtSignal(str)
+
+    def __init__(self, parent=None):
+        QDialog.__init__(self, parent)
+        self.setupUi(self)
+        self.printSignal.connect(self.printLog)
+
+    @pyqtSignature("")
+    def on_check_clicked(self):
+        self.textBrowser.setText("")
+        rootDir = self.lineEdit_4.text()
+        dir = unicode(rootDir, 'utf-8')
+        if os.path.exists(dir):
+            #self.checkDir(dir)
+            thread = threading.Thread(target=self.checkDir, args=(dir,))
+            thread.start()
+        else:
+            print rootDir
+            self.printSignal.emit(u"--------------- 亲，未找到该文件夹，请瞅瞅路径是否选对了呗！！--------------")
+
+    @pyqtSignature("")
+    def on_pick_clicked(self):
+        self.textBrowser.setText("")
+        excelPath = unicode(self.lineEdit_3.text(), 'utf-8')
+        src = unicode(self.lineEdit.text(), 'utf-8')
+        des = unicode(self.lineEdit_2.text(), 'utf-8')
+        if os.path.exists(src) and os.path.exists(excelPath) and os.path.exists(des):
+            # self.importExcel(excelPath, src, des)
+            thread = threading.Thread(target=self.importExcel, args=(excelPath, src, des))
+            thread.start()
+        else:
+            msg = u"---------------亲，未找到该文件夹，请瞅瞅路径是否选对了呗！！----------------"
+            self.printSignal.emit(msg)
+
+    @pyqtSignature("")
+    def on_file_clicked(self):
+        s = QtGui.QFileDialog.getExistingDirectory(self, u"选择文件夹", "/")
+        self.lineEdit.setText(unicode(s, "utf8"))
+
+    @pyqtSignature("")
+    def on_desfile_clicked(self):
+        s = QtGui.QFileDialog.getExistingDirectory(self, u"选择文件夹", "/")
+        self.lineEdit_2.setText(unicode(s, "utf8"))
+
+    @pyqtSignature("")
+    def on_excelfile_clicked(self):
+        s = QtGui.QFileDialog.getOpenFileName(self, u"选择文件", "/", '*.xlsx')
+        self.lineEdit_3.setText(unicode(s, "utf8"))
+
+
+    @pyqtSignature("")
+    def on_file_2_clicked(self):
+        s = QtGui.QFileDialog.getExistingDirectory(self, u"选择文件夹", "/")
+        self.lineEdit_4.setText(unicode(s, "utf8"))
+
+    def importExcel(self, excelPath, src, des):
+        count = 0
+        self.printSignal.emit(u"--------------------- 亲，图片挑选开始了！---------------------")
+        data = xlrd.open_workbook(excelPath)
+        table = data.sheets()[0]
+        for sku in table.col_values(0):
+            srcdir = os.path.join(src, sku)
+            desdir = os.path.join(des, sku)
+            count += self.moveFile(srcdir, desdir)
+        self.printSignal.emit(u"--------------------- 亲，图片挑选结束了，已经给您选择了%d条！！---------------------" % count)
+        os.startfile(des)
+
+    def moveFile(self, src, des):
+        result = 0
+        if os.path.exists(des):
+            shutil.rmtree(des)
+        if os.path.exists(src):
+            shutil.copytree(src, des)
+            msg = u"%s -> %s 移动成功！！" % (src, des)
+            self.printSignal.emit(msg)
+            result = 1
+        else:
+            msg = u"大亲啊，【%s】不存在，请瞅瞅Excel中数据是否正确呗！！！" % src
+            self.printSignal.emit(msg)
+        return result
+
+    def checkDir(self, rootDir):
+        self.printSignal.emit(u"--------------------- 亲，图片检测开始了！-----------------------")
+        list_dirs = os.walk(rootDir)
+        for root, dirs, files in list_dirs:
+            # for d in dirs:
+            #     print os.path.join(root, d)
+            for f in files:
+                if f.endswith('.jpg') or f.endswith('.png'):
+                    self.checkPic(os.path.join(root, f))
+                if f.endswith('.txt'):
+                    self.checkTxt(os.path.join(root, f))
+        self.printSignal.emit(u"--------------------- 亲，图片检测结束了！！---------------------")
+
+    def checkPic(self, PicPath):
+        file = os.path.basename(PicPath)
+        size = os.path.getsize(PicPath)
+        #返回图形对象
+        im = Image.open(PicPath)
+        #获取图片宽高
+        iw, ih = im.size
+
+        ##1-2-3-4-像素1100*1390（≤300KB）
+        if file.startswith("1") or file.startswith("2") or file.startswith("3") or file.startswith(
+                "4") or file.startswith("15"):
+            if size > 300 * 1024:
+                msg = u"%s 文件大小为%d，超出规定大小300KB " % (PicPath, size)
+                self.printSignal.emit(msg)
+            if iw > 1100 or ih > 1390:
+                msg = u"%s 文件像素为%d*%d，不符合规定大小1100*1390！" % (PicPath, iw, ih)
+                self.printSignal.emit(msg)
+
+        ##5-7-像素235*297 （≤30KB）
+        if file.startswith("5") or file.startswith("7"):
+            if size > 30 * 1024:
+                msg = u"%s文件大小为%d，超出规定大小300KB " % (PicPath, size)
+                self.printSignal.emit(msg)
+            if iw > 235 or ih > 297:
+                msg = u"%s 文件像素为%d*%d，不符合规定大小235*297！" % (PicPath, iw, ih)
+                self.printSignal.emit(msg)
+
+        ##6-像素750*10000px，≤2MB
+        if file.startswith("6"):
+            if size > 2 * 1024 * 1024:
+                msg = u"%s 文件大小为%d，超出规定大小2MB " % (PicPath, size)
+                self.printSignal.emit(msg)
+            if iw > 750 or ih > 10000:
+                msg = u" %s 文件像素为%d*%d，不符合规定大小750*10000！" % (PicPath, iw, ih)
+                self.printSignal.emit(msg)
+
+    def checkTxt(self, TxtPath):
+        file = open(TxtPath, 'r')
+        for (num, line) in enumerate(file):
+            if line.find("taobao.com") != -1 or line.find("tmall.com") != -1:
+                msg = u"%s 文件中第%d行包含字符串 taobao.com或者tmall.com，请检查！" % (TxtPath, num)
+                self.printSignal.emit(msg)
+        file.close()
+
+    def printLog(self, msg):
+        self.textBrowser.append(msg)
+        self.textBrowser.moveCursor(QtGui.QTextCursor.End)
+
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    ck = CheckTool()
+    ck.show()
+    sys.exit(app.exec_())
+    
+
---
+++ Ui_Checker.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\Users\Administrator\PycharmProjects\Checker\Checker.ui'
+#
+# Created: Wed Apr 01 12:26:13 2015
+#      by: PyQt4 UI code generator 4.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName(_fromUtf8("Dialog"))
+        Dialog.setEnabled(True)
+        Dialog.resize(676, 527)
+        Dialog.setMinimumSize(QtCore.QSize(676, 527))
+        Dialog.setMaximumSize(QtCore.QSize(676, 527))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(_fromUtf8("./picture/favicon.ico")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        Dialog.setWindowIcon(icon)
+        Dialog.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.textBrowser = QtGui.QTextBrowser(Dialog)
+        self.textBrowser.setGeometry(QtCore.QRect(10, 220, 651, 301))
+        self.textBrowser.setStyleSheet(_fromUtf8("background:url(./picture/MainForm.jpg)"))
+        self.textBrowser.setAcceptRichText(True)
+        self.textBrowser.setObjectName(_fromUtf8("textBrowser"))
+        self.label_2 = QtGui.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(20, 200, 71, 16))
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.tabWidget = QtGui.QTabWidget(Dialog)
+        self.tabWidget.setGeometry(QtCore.QRect(10, 20, 651, 171))
+        self.tabWidget.setStyleSheet(_fromUtf8("background:url(./picture/banner.png) no-repeat"))
+        self.tabWidget.setObjectName(_fromUtf8("tabWidget"))
+        self.tab = QtGui.QWidget()
+        self.tab.setObjectName(_fromUtf8("tab"))
+        self.check = QtGui.QPushButton(self.tab)
+        self.check.setGeometry(QtCore.QRect(450, 20, 75, 23))
+        self.check.setObjectName(_fromUtf8("check"))
+        self.splitter_4 = QtGui.QSplitter(self.tab)
+        self.splitter_4.setGeometry(QtCore.QRect(20, 20, 291, 23))
+        self.splitter_4.setOrientation(QtCore.Qt.Horizontal)
+        self.splitter_4.setObjectName(_fromUtf8("splitter_4"))
+        self.label_5 = QtGui.QLabel(self.splitter_4)
+        self.label_5.setObjectName(_fromUtf8("label_5"))
+        self.lineEdit_4 = QtGui.QLineEdit(self.splitter_4)
+        self.lineEdit_4.setObjectName(_fromUtf8("lineEdit_4"))
+        self.file_2 = QtGui.QPushButton(self.splitter_4)
+        self.file_2.setObjectName(_fromUtf8("file_2"))
+        self.tabWidget.addTab(self.tab, _fromUtf8(""))
+        self.tab_2 = QtGui.QWidget()
+        self.tab_2.setObjectName(_fromUtf8("tab_2"))
+        self.pick = QtGui.QPushButton(self.tab_2)
+        self.pick.setGeometry(QtCore.QRect(450, 20, 75, 23))
+        self.pick.setObjectName(_fromUtf8("pick"))
+        self.splitter_2 = QtGui.QSplitter(self.tab_2)
+        self.splitter_2.setGeometry(QtCore.QRect(20, 60, 301, 23))
+        self.splitter_2.setOrientation(QtCore.Qt.Horizontal)
+        self.splitter_2.setObjectName(_fromUtf8("splitter_2"))
+        self.label_3 = QtGui.QLabel(self.splitter_2)
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.lineEdit_2 = QtGui.QLineEdit(self.splitter_2)
+        self.lineEdit_2.setObjectName(_fromUtf8("lineEdit_2"))
+        self.desfile = QtGui.QPushButton(self.splitter_2)
+        self.desfile.setObjectName(_fromUtf8("desfile"))
+        self.splitter_3 = QtGui.QSplitter(self.tab_2)
+        self.splitter_3.setGeometry(QtCore.QRect(20, 100, 301, 23))
+        self.splitter_3.setOrientation(QtCore.Qt.Horizontal)
+        self.splitter_3.setObjectName(_fromUtf8("splitter_3"))
+        self.label_4 = QtGui.QLabel(self.splitter_3)
+        self.label_4.setObjectName(_fromUtf8("label_4"))
+        self.lineEdit_3 = QtGui.QLineEdit(self.splitter_3)
+        self.lineEdit_3.setObjectName(_fromUtf8("lineEdit_3"))
+        self.excelfile = QtGui.QPushButton(self.splitter_3)
+        self.excelfile.setObjectName(_fromUtf8("excelfile"))
+        self.splitter = QtGui.QSplitter(self.tab_2)
+        self.splitter.setGeometry(QtCore.QRect(20, 20, 301, 23))
+        self.splitter.setOrientation(QtCore.Qt.Horizontal)
+        self.splitter.setObjectName(_fromUtf8("splitter"))
+        self.label = QtGui.QLabel(self.splitter)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.lineEdit = QtGui.QLineEdit(self.splitter)
+        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
+        self.file = QtGui.QPushButton(self.splitter)
+        self.file.setObjectName(_fromUtf8("file"))
+        self.tabWidget.addTab(self.tab_2, _fromUtf8(""))
+        self.label_6 = QtGui.QLabel(Dialog)
+        self.label_6.setGeometry(QtCore.QRect(400, 10, 211, 21))
+        self.label_6.setObjectName(_fromUtf8("label_6"))
+
+        self.retranslateUi(Dialog)
+        self.tabWidget.setCurrentIndex(0)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(_translate("Dialog", "韩都衣舍活动管理图片筛选器", None))
+        self.textBrowser.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'SimSun\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", None))
+        self.label_2.setText(_translate("Dialog", "检测结果", None))
+        self.check.setText(_translate("Dialog", "检测图片", None))
+        self.label_5.setText(_translate("Dialog", "文件目录", None))
+        self.lineEdit_4.setText(_translate("Dialog", "请选择图片目录", None))
+        self.file_2.setText(_translate("Dialog", "选择文件夹", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("Dialog", "检测图片", None))
+        self.pick.setText(_translate("Dialog", "图片挑选", None))
+        self.label_3.setText(_translate("Dialog", "文件目录", None))
+        self.lineEdit_2.setText(_translate("Dialog", "请选择保存文件夹", None))
+        self.desfile.setText(_translate("Dialog", "选择文件夹", None))
+        self.label_4.setText(_translate("Dialog", "选择Excel", None))
+        self.lineEdit_3.setText(_translate("Dialog", "请选择货号文件夹", None))
+        self.excelfile.setText(_translate("Dialog", "选择文件", None))
+        self.label.setText(_translate("Dialog", "文件目录", None))
+        self.lineEdit.setText(_translate("Dialog", "请选择图片文件夹", None))
+        self.file.setText(_translate("Dialog", "选择文件夹", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("Dialog", "挑选图片", None))
+        self.label_6.setText(_translate("Dialog", "Gana项目组制作 ，请给红心哈 ^_^！", None))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    Dialog = QtGui.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
+
